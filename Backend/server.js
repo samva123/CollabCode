02ec7d7b3c9080a70e7bfddc410ca4386bf9f36c
Tejkaur04 <tejinderkaur354@gmail.com>
2025-11-24 12:42:42 +0530

    deployed

--- Backend/server.js
+++ Backend/server.js
@@ -11,7 +11,7 @@ const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server, { cors: { origin: "*" } });
+const io = new Server(server, { cors: { origin: "https://collab-code-virid.vercel.app/" } });
 
 // ----------------- Middleware -----------------
 app.use(cors());
